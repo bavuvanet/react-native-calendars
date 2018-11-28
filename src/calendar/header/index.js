effc69c6fa7f0bde7644ddf3c42b0335b6220a5f
BaVV <bavuvanet@gmail.com>
2018-11-28 15:23:08 +0700

    Update index.js

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -122,12 +122,12 @@ class CalendarHeader extends Component {
       <View>
         <View style={this.style.header}>
           {leftArrow}
-          <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity activeOpacity={0.7} onPress={this.props.onPressDate} style={{ flexDirection: 'row' }}>
             <Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>
               {this.props.month.toString(this.props.monthFormat)}
             </Text>
             {indicator}
-          </View>
+          </TouchableOpacity>
           {rightArrow}
         </View>
         {
